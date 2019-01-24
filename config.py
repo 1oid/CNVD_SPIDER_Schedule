682c45d638886db33f0ae629d4c824743a52c04f
1oid <1398231927@qq.com>
2019-01-24 19:20:44 +0800

    first commit

---
+++ config.py
@@ -0,0 +1,20 @@
+# 定时的时间
+SCHEDULE_TASK_DAY_AT = [
+    "19:06",
+    "19:07",
+    "19:08"
+]
+
+# 发信邮件
+SMTP_USER = ""
+SMTP_PASS = ""
+
+# 接受者
+RECEIVERS = [
+    "heisying@163.com",
+    "2563152103@qq.com"
+]
+
+# 每多久进行一次循环
+LOOP_SLEEP = 30
+
